b6f1cc4b418251668572dfec63aaad7d62ad76c6
Diego <diegofelipe1025@gmail.com>
2025-11-01 14:42:53 -0300

    fix:adding the Lombok annotation

--- src/main/java/com/github/diegof856/ListaContatos/entity/Address.java
+++ src/main/java/com/github/diegof856/ListaContatos/entity/Address.java
@@ -1,6 +1,8 @@
 package com.github.diegof856.ListaContatos.entity;
 
+import lombok.Data;
 
+@Data
 public class Address {
     private String street;
     private String number;
@@ -11,39 +13,6 @@ public class Address {
         this.street = street;
         this.number = number;
         this.city = city;
-        this.state = state;
-    }
-
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
         this.state = state;
     }
 }
--- src/main/java/com/github/diegof856/ListaContatos/entity/Contact.java
+++ src/main/java/com/github/diegof856/ListaContatos/entity/Contact.java
@@ -1,5 +1,6 @@
 package com.github.diegof856.ListaContatos.entity;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -9,6 +10,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @Document(collection = "contact")
+@Data
 public class Contact {
     @Id
     private UUID id;
@@ -24,53 +26,5 @@ public class Contact {
         this.email = email;
         this.name = name;
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public List<Address> getAddresses() {
-        return addresses;
-    }
-
-    public void setAddresses(List<Address> addresses) {
-        this.addresses = addresses;
     }
 }
